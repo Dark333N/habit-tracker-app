e9e7c1a5d9ebef6cf02a7eed624ed6e028253d05
Dark333N <75328590+Dark333N@users.noreply.github.com>
2025-09-10 21:34:32 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,11 @@
-import { setupIcons, setActiveIcon, renderHabits } from "./ui.js";
-import { addHabit, setHabitCompleted } from "./database.js";
+import { setupIcons, setActiveIcon, renderHabits, renderOverdueAndDone, closeHabitModal, openHabitModal} from "./ui.js";
+import { addHabit, deleteHabit, setHabitCompleted, getOverdueHabits, getDoneHabits} from "./database.js";
 
-// Initialize icons
+
+
+/****************************************************
+                    Navigation and Icons
+  ****************************************************/
 setupIcons();
 
 // Navigation handling
@@ -17,6 +21,16 @@ buttons.forEach(btn => {
     });
     document.getElementById(screenId).classList.add("active");
 
+    // Render done and overdue habits when switching to those screens
+    if (screenId === "overdue-screen") {
+      renderOverdueAndDone(false);
+    }
+
+    if (screenId === "done-screen") {
+      renderOverdueAndDone(true);
+    }
+
+    // Set active icon
     const activeTabBtn = document.querySelector(`.tab-bar button[data-screen="${screenId}"]`);
     if (activeTabBtn) {
       setActiveIcon(activeTabBtn);
@@ -36,6 +50,73 @@ buttons.forEach(btn => {
   });
 });
 
+
+/*****************************************************
+                    Form Handling
+  ****************************************************/
+
+document.getElementById("add-habit-form").addEventListener("submit", async e => {
+  e.preventDefault();
+  console.log("Form submitted");
+
+  const habitName = document.getElementById("habit-name").value.trim();
+  const habitDate = document.getElementById("habit-date").value;
+  const habitType = document.getElementById("habit-type").value.trim();
+  const habitMinutes = document.getElementById("habit-minutes").value || null;
+  const habitInfo = document.getElementById("habit-info").value.trim() || "";
+
+
+  let habitPicture = null;
+  const fileInput = document.getElementById("habit-picture").files[0] || null;
+
+  if (fileInput) {
+    habitPicture = await resizeAndCompress(fileInput, 300, 0.9);
+  }
+
+
+  if (!habitName || !habitDate || !habitType) {
+    alert("Please fill in all required fields.");
+    return;
+  }
+
+  await addHabit(habitDate, habitName, habitType, habitMinutes, false, habitInfo, habitPicture);
+
+  e.target.reset();
+
+  document.querySelectorAll(".screen").forEach(screen => {
+    screen.classList.remove("active");
+  });
+  document.getElementById("home-screen").classList.add("active");
+
+  renderHabits(getNext7Days());
+});
+
+
+/*****************************************************
+                    Rendering Helpers
+  ****************************************************/
+
+renderHabits(getNext7Days());
+updateCounters();
+
+
+
+// Rerender current screen
+export function rerenderCurrentScreen(){
+  const activeScreen = document.querySelector(".screen.active");
+  if (activeScreen.id === "home-screen") {
+    renderHabits(getNext7Days());
+  }
+  else if (activeScreen.id === "overdue-screen") {
+    renderOverdueAndDone(false);
+  }
+  else if (activeScreen.id === "done-screen") {
+    renderOverdueAndDone(true);
+  }
+
+  // Update overdue and done counts
+  updateCounters();
+}
 
 // Get next 7 days
 
@@ -74,54 +155,60 @@ async function resizeAndCompress(file, maxSize=300, quality=0.9) {
   });
 }
 
-// form submit
 
-document.getElementById("add-habit-form").addEventListener("submit", async e => {
-  e.preventDefault();
-  console.log("Form submitted");
+// Update counters for overdue and done habits
+async function updateCounters() {
+  const overdueH = await getOverdueHabits();
+  const doneH = await getDoneHabits();
 
-  const habitName = document.getElementById("habit-name").value.trim();
-  const habitDate = document.getElementById("habit-date").value;
-  const habitType = document.getElementById("habit-type").value.trim();
-  const habitMinutes = document.getElementById("habit-minutes").value || null;
-  const habitInfo = document.getElementById("habit-info").value.trim() || "";
+  document.getElementById("overdue-count").textContent = overdueH.length;
+  document.getElementById("done-count").textContent = doneH.length;
+}
 
 
-  let habitPicture = null;
-  const fileInput = document.getElementById("habit-picture").files[0] || null;
+/*****************************************************
+                    Button Handling
+  ****************************************************/
 
-  if (fileInput) {
-    habitPicture = await resizeAndCompress(fileInput, 300, 0.9);
-  }
+// Setup check button handling
+
+function setupCheckButtonHandler(containerId) {
+  const container = document.getElementById(containerId);
+  container.addEventListener("click", async e => {
+    if (e.target.classList.contains("habit-check-btn")) {
+      const id = Number(e.target.dataset.id);
+      const completed = e.target.dataset.completed === "true";
+      await setHabitCompleted(id, !completed);
+      rerenderCurrentScreen();
+    }
+  })
+}
+
+setupCheckButtonHandler("habits-container");
+setupCheckButtonHandler("overdue-habits-container");
+setupCheckButtonHandler("done-habits-container");
 
 
-  if (!habitName || !habitDate || !habitType) {
-    alert("Please fill in all required fields.");
-    return;
-  }
+// Setup delete button handling
 
-  await addHabit(habitDate, habitName, habitType, habitMinutes, habitInfo, habitPicture);
-
-  e.target.reset();
-
-  document.querySelectorAll(".screen").forEach(screen => {
-    screen.classList.remove("active");
-  });
-  document.getElementById("home-screen").classList.add("active");
-
-  renderHabits(getNext7Days());
+const deletBtn = document.getElementById("delete-habit-btn");
+deletBtn.addEventListener("click", async () => {
+  const id = Number(deletBtn.dataset.id);
+  await deleteHabit(id);
+  closeHabitModal();
+  rerenderCurrentScreen();
 });
 
-renderHabits(getNext7Days());
+const closeBtn = document.getElementById("modal-close-btn");
+closeBtn.addEventListener("click", () => {
+  closeHabitModal();
+});
 
+// Open habit modal
 
-// Set habit completed on click of check button
-document.getElementById("habits-container").addEventListener("click", async e => {
-  if (e.target.classList.contains("habit-check-btn")) {
+document.body.addEventListener("click", e => {
+  if (e.target.classList.contains("habit-name")) {
     const id = Number(e.target.dataset.id);
-    const completed = e.target.dataset.completed === "true";
-
-    await setHabitCompleted(id, !completed);
-    renderHabits(getNext7Days());
+    openHabitModal(id);
   }
 });
